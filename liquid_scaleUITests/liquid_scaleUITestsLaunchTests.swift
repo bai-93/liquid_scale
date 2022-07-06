2ca6a634ec60d3a5813cee0a6bf534080c1aa65d
bai-93 <abtwmreal@gmail.com>
2022-07-06 16:58:41 +0600

    Initial Commit

---
+++ liquid_scaleUITests/liquid_scaleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  liquid_scaleUITestsLaunchTests.swift
+//  liquid_scaleUITests
+//
+//  Created by Baiaman Apsamatov on 6/7/22.
+//
+
+import XCTest
+
+class liquid_scaleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
